35b58b4fb35540722121600784692ae54707cd44
yasmeen1999 <yasmeenaleemkhan786@gmail.com>
2020-11-29 13:35:43 +0530

    nextjs routing

---
+++ pages/about.js
@@ -0,0 +1,21 @@
+// import Header from './header';
+
+// const About = () => {
+//     return (  
+//         <>
+//            <Header/>
+//         <h1 classname="heading">About</h1>
+//         <style jsx>
+//             {
+                
+//             }
+//         </style>
+//         </>
+     
+//     );
+// }
+
+
+ 
+// export default About;
+
